dece6ee9a040a96b1bc83931c20bddbf9eb7a77c
Timo Leitritz <42964574+TimoIllusion@users.noreply.github.com>
2024-11-16 15:49:30 +0100

    Simplify code for cpp inference

--- inference_tensorrt10_cpp/main.cpp
+++ inference_tensorrt10_cpp/main.cpp
@@ -16,21 +16,27 @@
 #include "NvOnnxParser.h"
 #include <opencv2/opencv.hpp>
 
-#include "util.h"
-#include "logger.h"
 #include "prepostprocessing.h" // Header file for preprocessing and postprocessing functions
+
+// Simple TensorRT Logger Class
+class SimpleLogger : public nvinfer1::ILogger {
+public:
+    void log(nvinfer1::ILogger::Severity severity, const char* msg) noexcept override {
+        std::cerr << "[TensorRT] " << msg << std::endl;
+    }
+};
 
 class TRTInference
 {
 public:
-    TRTInference(const std::string& engineFilename);
+    TRTInference(const std::string& engineFilename, nvinfer1::ILogger& logger);
     bool infer(const std::vector<float>& input_buffer, std::vector<float>& output_buffer);
 
 private:
     std::string mEngineFilename;                    
     nvinfer1::Dims mInputDims;                      
     nvinfer1::Dims mOutputDims;                     
-    std::unique_ptr<TRTLogger> mLogger;             
+    nvinfer1::ILogger& mLogger;              
     std::unique_ptr<nvinfer1::IRuntime> mRuntime;  
     std::unique_ptr<nvinfer1::ICudaEngine> mEngine;
     std::unique_ptr<nvinfer1::IExecutionContext> mContext; // Execution context for inference
@@ -41,11 +47,12 @@ private:
     cudaStream_t mStream; // CUDA stream
 
     void allocateResources(int32_t width, int32_t height);
+    static size_t getMemorySize(const nvinfer1::Dims& dims, const int32_t elem_size);
 };
 
-TRTInference::TRTInference(const std::string& engineFilename)
+TRTInference::TRTInference(const std::string& engineFilename, nvinfer1::ILogger& logger)
     : mEngineFilename(engineFilename)
-    , mEngine(nullptr), mLogger(new TRTLogger(nvinfer1::ILogger::Severity::kINFO))
+    , mEngine(nullptr), mLogger(logger)
 {
     std::ifstream engineFile(engineFilename, std::ios::binary);
     if (engineFile.fail())
@@ -60,7 +67,7 @@ TRTInference::TRTInference(const std::string& engineFilename)
     std::vector<char> engineData(fsize);
     engineFile.read(engineData.data(), fsize);
 
-    mRuntime.reset(nvinfer1::createInferRuntime(*mLogger));
+    mRuntime.reset(nvinfer1::createInferRuntime(mLogger));
     mEngine.reset(mRuntime->deserializeCudaEngine(engineData.data(), fsize));
     assert(mEngine.get() != nullptr);
 
@@ -78,12 +85,12 @@ void TRTInference::allocateResources(int32_t width, int32_t height)
     assert(mEngine->getTensorDataType(input_name) == nvinfer1::DataType::kFLOAT);
     auto input_dims = nvinfer1::Dims4{1, 3, height, width};
     mContext->setInputShape(input_name, input_dims);
-    mInputSize = util::getMemorySize(input_dims, sizeof(float));
+    mInputSize = getMemorySize(input_dims, sizeof(float));
 
     char const* output_name = "output";
     assert(mEngine->getTensorDataType(output_name) == nvinfer1::DataType::kFLOAT);
     auto output_dims = mContext->getTensorShape(output_name);
-    mOutputSize = util::getMemorySize(output_dims, sizeof(float));
+    mOutputSize = getMemorySize(output_dims, sizeof(float));
 
     // Allocate pinned memory for input and device memory for output
     cudaHostAlloc(&mInputMem, mInputSize, cudaHostAllocDefault);  // Allocate pinned host memory for input
@@ -116,12 +123,18 @@ bool TRTInference::infer(const std::vector<float>& input_buffer, std::vector<flo
     return true;
 }
 
+size_t TRTInference::getMemorySize(const nvinfer1::Dims& dims, const int32_t elem_size)
+{
+    return std::accumulate(dims.d, dims.d + dims.nbDims, 1, std::multiplies<int64_t>()) * elem_size;
+}
+
 int main(int argc, char** argv)
 {
     int32_t width{224};
     int32_t height{224};
 
-    TRTInference sample("efficientnet.engine");
+    SimpleLogger logger;
+    TRTInference sample("efficientnet.engine", logger);
 
     std::cout << "Running TensorRT inference" << std::endl;
 
